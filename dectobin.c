878adf80c1829ce3fffb69d33eef2500701d7f13
dharanipriya <dharuchithu@gmail.com>
2016-07-20 20:05:05 +0530

    Create dectobin.c

---
+++ dectobin.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int dec,bin[200],i=0,q;
+clrscr();
+scanf("\n %d",&dec);
+dec=q;
+while(q!==0)
+{
+bin[i++]=qq%2;
+q=q/2;
+}
+while(i--)
+{
+printf("\n %d",bin[i]);
+return 0;
+}
+getch();
+}
